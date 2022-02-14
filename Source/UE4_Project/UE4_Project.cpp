5b736990d7668e8dca3e8cef16ffe56c8edc9a36
ChickAnyJay <vgb1960@naver.com>
2022-02-14 23:48:41 +0900

    Add Log Macro for simple call

--- Source/UE4_Project/UE4_Project.cpp
+++ Source/UE4_Project/UE4_Project.cpp
@@ -3,4 +3,5 @@
 #include "UE4_Project.h"
 #include "Modules/ModuleManager.h"
 
+DEFINE_LOG_CATEGORY(DM_UE4);
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4_Project, "UE4_Project" );
--- Source/UE4_Project/UE4_Project.h
+++ Source/UE4_Project/UE4_Project.h
@@ -4,3 +4,8 @@
 
 #include "CoreMinimal.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(DM_UE4, Log, All);
+
+#define MYLOG_CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+#define MYLOG_S() UE_LOG(DM_UE4, Warning, TEXT("%s"), *MYLOG_CALLINFO)
+#define MYLOG(Format, ...) UE_LOG(DM_UE4, Warning, TEXT("%s %s"), *MYLOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
